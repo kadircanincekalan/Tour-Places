af8e8e2e6c7ff0eae597c2dd5ee55b642caca9d3
kadircanincekalan <kadircanincekalan@gmail.com>
2022-10-02 15:06:34 +0300

    header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
+
+const App = () =>{
+  return(
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      
+    </div>
+  )
+}
+
+export default App;
